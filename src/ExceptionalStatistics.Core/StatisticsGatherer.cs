3fc16fce2c2d1b5660b2e96ac7f65b99c2276e78
Jason Bock <jason.r.bock@outlook.com>
2022-02-10 21:09:25 -0600

    Updated StatisticsGatherer to handle all expressions and statements, and got the program running against Roslyn

--- src/ExceptionalStatistics.Core/StatisticsGatherer.cs
+++ src/ExceptionalStatistics.Core/StatisticsGatherer.cs
@@ -22,6 +22,7 @@ public sealed class StatisticsGatherer
 			(expressionsCount, statementsCount, badCatchBlocksCount) =
 				(this.ExpressionsCount, this.StatementsCount, this.BadCatchBlocksCount);
 
+		#region Statements
 		public override void VisitExpressionStatement(ExpressionStatementSyntax node)
 		{
 			this.StatementsCount++;
@@ -82,11 +83,386 @@ public sealed class StatisticsGatherer
 			base.VisitForStatement(node);
 		}
 
+		public override void VisitGlobalStatement(GlobalStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitGlobalStatement(node);
+		}
+
+		public override void VisitGotoStatement(GotoStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitGotoStatement(node);
+		}
+
+		public override void VisitIfStatement(IfStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitIfStatement(node);
+		}
+
+		public override void VisitLabeledStatement(LabeledStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitLabeledStatement(node);
+		}
+
+		public override void VisitLocalDeclarationStatement(LocalDeclarationStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitLocalDeclarationStatement(node);
+		}
+
+		public override void VisitLocalFunctionStatement(LocalFunctionStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitLocalFunctionStatement(node);
+		}
+
+		public override void VisitLockStatement(LockStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitLockStatement(node);
+		}
+
+		public override void VisitReturnStatement(ReturnStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitReturnStatement(node);
+		}
+
+		public override void VisitSwitchStatement(SwitchStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitSwitchStatement(node);
+		}
+
+		public override void VisitThrowStatement(ThrowStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitThrowStatement(node);
+		}
+
+		public override void VisitTryStatement(TryStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitTryStatement(node);
+		}
+
+		public override void VisitUnsafeStatement(UnsafeStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitUnsafeStatement(node);
+		}
+
+		public override void VisitUsingStatement(UsingStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitUsingStatement(node);
+		}
+
+		public override void VisitWhileStatement(WhileStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitWhileStatement(node);
+		}
+
+		public override void VisitYieldStatement(YieldStatementSyntax node)
+		{
+			this.StatementsCount++;
+			base.VisitYieldStatement(node);
+		}
+		#endregion
+
+		#region Expressions
+		public override void VisitAnonymousMethodExpression(AnonymousMethodExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitAnonymousMethodExpression(node);
+		}
+
+		public override void VisitAnonymousObjectCreationExpression(AnonymousObjectCreationExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitAnonymousObjectCreationExpression(node);
+		}
+
+		public override void VisitArrayCreationExpression(ArrayCreationExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitArrayCreationExpression(node);
+		}
+
+		public override void VisitArrowExpressionClause(ArrowExpressionClauseSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitArrowExpressionClause(node);
+		}
+
+		public override void VisitAssignmentExpression(AssignmentExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitAssignmentExpression(node);
+		}
+
+		public override void VisitAwaitExpression(AwaitExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitAwaitExpression(node);
+		}
+
+		public override void VisitBaseExpression(BaseExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitBaseExpression(node);
+		}
+
+		public override void VisitBinaryExpression(BinaryExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitBinaryExpression(node);
+		}
+
+		public override void VisitCastExpression(CastExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitCastExpression(node);
+		}
+
+		public override void VisitCheckedExpression(CheckedExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitCheckedExpression(node);
+		}
+
+		public override void VisitConditionalAccessExpression(ConditionalAccessExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitConditionalAccessExpression(node);
+		}
+
+		public override void VisitConditionalExpression(ConditionalExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitConditionalExpression(node);
+		}
+
+		public override void VisitDeclarationExpression(DeclarationExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitDeclarationExpression(node);
+		}
+
+		public override void VisitDefaultExpression(DefaultExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitDefaultExpression(node);
+		}
+
+		public override void VisitElementAccessExpression(ElementAccessExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitElementAccessExpression(node);
+		}
+
+		public override void VisitElementBindingExpression(ElementBindingExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitElementBindingExpression(node);
+		}
+
+		public override void VisitImplicitArrayCreationExpression(ImplicitArrayCreationExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitImplicitArrayCreationExpression(node);
+		}
+
+		public override void VisitImplicitObjectCreationExpression(ImplicitObjectCreationExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitImplicitObjectCreationExpression(node);
+		}
+
+		public override void VisitImplicitStackAllocArrayCreationExpression(ImplicitStackAllocArrayCreationExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitImplicitStackAllocArrayCreationExpression(node);
+		}
+
+		public override void VisitInitializerExpression(InitializerExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitInitializerExpression(node);
+		}
+
+		public override void VisitInterpolatedStringExpression(InterpolatedStringExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitInterpolatedStringExpression(node);
+		}
+
+		public override void VisitInvocationExpression(InvocationExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitInvocationExpression(node);
+		}
+
+		public override void VisitIsPatternExpression(IsPatternExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitIsPatternExpression(node);
+		}
+
+		public override void VisitLiteralExpression(LiteralExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitLiteralExpression(node);
+		}
+
+		public override void VisitMakeRefExpression(MakeRefExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitMakeRefExpression(node);
+		}
+
+		public override void VisitMemberAccessExpression(MemberAccessExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitMemberAccessExpression(node);
+		}
+
+		public override void VisitMemberBindingExpression(MemberBindingExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitMemberBindingExpression(node);
+		}
+
+		public override void VisitObjectCreationExpression(ObjectCreationExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitObjectCreationExpression(node);
+		}
+
+		public override void VisitOmittedArraySizeExpression(OmittedArraySizeExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitOmittedArraySizeExpression(node);
+		}
+
+		public override void VisitParenthesizedExpression(ParenthesizedExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitParenthesizedExpression(node);
+		}
+
 		public override void VisitParenthesizedLambdaExpression(ParenthesizedLambdaExpressionSyntax node)
 		{
 			this.ExpressionsCount++;
 			base.VisitParenthesizedLambdaExpression(node);
 		}
+
+		public override void VisitPostfixUnaryExpression(PostfixUnaryExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitPostfixUnaryExpression(node);
+		}
+
+		public override void VisitPrefixUnaryExpression(PrefixUnaryExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitPrefixUnaryExpression(node);
+		}
+
+		public override void VisitQueryExpression(QueryExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitQueryExpression(node);
+		}
+
+		public override void VisitRangeExpression(RangeExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitRangeExpression(node);
+		}
+
+		public override void VisitRefExpression(RefExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitRefExpression(node);
+		}
+
+		public override void VisitRefTypeExpression(RefTypeExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitRefTypeExpression(node);
+		}
+
+		public override void VisitRefValueExpression(RefValueExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitRefValueExpression(node);
+		}
+
+		public override void VisitSimpleLambdaExpression(SimpleLambdaExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitSimpleLambdaExpression(node);
+		}
+
+		public override void VisitSizeOfExpression(SizeOfExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitSizeOfExpression(node);
+		}
+
+		public override void VisitStackAllocArrayCreationExpression(StackAllocArrayCreationExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitStackAllocArrayCreationExpression(node);
+		}
+
+		public override void VisitSwitchExpression(SwitchExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitSwitchExpression(node);
+		}
+
+		public override void VisitSwitchExpressionArm(SwitchExpressionArmSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitSwitchExpressionArm(node);
+		}
+
+		public override void VisitThisExpression(ThisExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitThisExpression(node);
+		}
+
+		public override void VisitThrowExpression(ThrowExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitThrowExpression(node);
+		}
+
+		public override void VisitTupleExpression(TupleExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitTupleExpression(node);
+		}
+
+		public override void VisitTypeOfExpression(TypeOfExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitTypeOfExpression(node);
+		}
+
+		public override void VisitWithExpression(WithExpressionSyntax node)
+		{
+			this.ExpressionsCount++;
+			base.VisitWithExpression(node);
+		}
+		#endregion
 
 		public override void VisitCatchClause(CatchClauseSyntax node)
 		{
@@ -94,8 +470,18 @@ public sealed class StatisticsGatherer
 			// and either it doesn't have a CatchDeclarationSyntax child
 			// or the name of the CatchDeclarationSyntax child
 			// is either "Exception" or "System.Exception"
+
 			// TODO: I may have to try and "get" a SemanticModel
 			// to determine exactly what the exception type is.
+
+			// TODO: I want to capture where this is happening
+			// So, I may have to recursively look at the parents
+			// and look for a node that is of type MemberDeclarationSyntax
+			// (maybe I can be specific and say MethodDeclarationSyntax)
+			// Maybe I just keep a running List<CatchClauseSyntax> and
+			// provide an ImmutableArray<CatchClauseSyntax> as a getter
+			// that I can look at later.
+
 			if (!node.Block.DescendantNodes(_ => true).Any())
 			{
 				var catchDeclaration = node.DescendantNodes().OfType<CatchDeclarationSyntax>().SingleOrDefault();
--- src/ExceptionalStatistics/Program.cs
+++ src/ExceptionalStatistics/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ExceptionalStatistics.Core;
+
+var statistics = new Statistics();
+var fileCount = 0;
+
+// "M:\\repos\\roslyn"
+// "C:\\Users\\jason\\source\\repos\\ExceptionalStatistics\\src"
+foreach (var file in Directory.EnumerateFiles("M:\\repos\\roslyn", "*.cs", SearchOption.AllDirectories))
+{
+	fileCount++;
+	await Console.Out.WriteLineAsync($"Processing {file}...").ConfigureAwait(false);
+	statistics += new StatisticsGatherer(await File.ReadAllTextAsync(file).ConfigureAwait(false));
+}
+
+await Console.Out.WriteLineAsync($"Total number of files: {fileCount}").ConfigureAwait(false);
+await Console.Out.WriteLineAsync($"Number of statements: {statistics.StatementsCount}").ConfigureAwait(false);
+await Console.Out.WriteLineAsync($"Number of expressions: {statistics.ExpressionsCount}").ConfigureAwait(false);
+await Console.Out.WriteLineAsync($"Number of bad catch blocks: {statistics.BadCatchBlocksCount}").ConfigureAwait(false);
+
+internal sealed class Statistics
+{
+	internal Statistics() { }
+
+	public static Statistics operator +(Statistics left, StatisticsGatherer right) =>
+		new(left.BadCatchBlocksCount + right.BadCatchBlocksCount,
+			left.ExpressionsCount + right.ExpressionsCount,
+			left.StatementsCount + right.StatementsCount);
+
+	private Statistics(uint badCatchBlocksCount, uint expressionsCount, uint statementsCount) =>
+		(this.BadCatchBlocksCount, this.ExpressionsCount, this.StatementsCount) =
+			(badCatchBlocksCount, expressionsCount, statementsCount);
+
+	internal uint BadCatchBlocksCount { get; }
+	internal uint ExpressionsCount { get; }
+	internal uint StatementsCount { get; }
+}
